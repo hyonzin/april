2bd38e846429877013385d5b8961e16a0c1aa23b
hyonzin <hyeonjin507@gmail.com>
2021-04-02 03:27:43 +0900

    workflow pull test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World 💙
+          Hello World ❤️
         </p>
         <a
           className="App-link"
