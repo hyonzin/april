97f02137d39f39dec51a2524a4e1be6f105de3d5
hyonzin <hyeonjin507@gmail.com>
2021-04-02 02:50:57 +0900

    puller: can use docker cli

--- script/deploy-puller.sh
+++ script/deploy-puller.sh
@@ -7,6 +7,7 @@ docker build --tag april-puller-image stub/puller
 docker run -d --rm \
 		   -v "${PWD}"/:/app/ \
 		   -v ~/.ssh:/root/.ssh \
+		   -v /var/run/docker.sock:/var/run/docker.sock \
 		   -p 9999:9999 \
 		   --name april-puller \
 		   april-puller-image
